5305abb084bf7907767ead4450e2e97eaca57aef
darko <kolevdarko@gmail.com>
2017-06-29 17:02:46 +0200

    Added basic models

--- meals/apps/base/models.py
+++ meals/apps/base/models.py
@@ -1,4 +1,22 @@
 """Base models"""
+from django.db import models
 
-class Restoran():
-    pass
+
+class Restoran(models.Model):
+    name = models.TextField(max_length=50)
+    description = models.TextField(max_length=500)
+
+class Menu(models.Model):
+    name = models.TextField(max_length=50)
+    restoran = models.ForeignKey(Restoran, on_delete=models.CASCADE)
+
+class Meal(models.Model):
+    name = models.TextField(max_length=50)
+    description = models.TextField(max_length=500)
+    menu = models.ForeignKey(Menu, related_name='meals', on_delete=models.CASCADE)
+
+
+
+
+
+
